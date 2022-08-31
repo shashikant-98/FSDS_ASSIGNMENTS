d380bca6e1e3276b4c474944c2c40842258a5cc5
shashikant-98 <77399480+shashikant-98@users.noreply.github.com>
2022-08-31 11:24:45 +0530

    Add files via upload

---
+++ FSDS_PROGRAMMING_ASSIGNMENT_01.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Write a python program to print "Hello World"?
+
+
+# In[2]:
+
+
+print("Hello World")
+
+
+# In[4]:
+
+
+# Write a python program to do arithmetic operations addition and division?
+a = 12
+b = 3
+add = a+b
+print(a+b)
+div = a/b
+print(a/b)
+
+
+# In[10]:
+
+
+# Write a python program to find area of a triangle?
+# Base of triangle = b
+# Height of Triangle = h
+# Area of Triangle = A
+b = 12
+h = 4
+A = 0.5 * b * h
+print(A)
+
+
+# In[15]:
+
+
+#Write a python program to swap two variables?
+a = 10
+b = 20
+x = a
+a = b
+b = x
+print (a,b)
+
+
+# In[18]:
+
+
+# Write a python program to generate a random number?
+import random
+n = random.random()
+print (n)
+
+
+# In[ ]:
+
+
+
+
