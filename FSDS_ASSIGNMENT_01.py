08189c0b8568db7aec4a0231daee60abbb4a6bc2
shashikant-98 <77399480+shashikant-98@users.noreply.github.com>
2022-09-02 12:01:57 +0530

    Add files via upload

---
+++ FSDS_ASSIGNMENT_01.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:Q1.In the below elements which of them are values or an expression? eg:- values can be integer or string and expressions will be mathematical operators.
+
+#Ans:
+# * - expression
+# 'hello'- value
+# -87.8 - value
+# - -expression
+# / -expression
+# + -expression
+# 6 -value
+
+
+# In[2]:Q2. What is the difference bw string and variables?
+
+#Ans: 
+#Variables : variables are the symbols that we use to store data in a program.
+#Strings : Strings are the data that are filled in variables.
+
+
+# In[3]:Q3.Describe three different data types?
+
+#Ans:
+#Three different data types are:
+# 1.string : text/number anything bw "" is considered as string data type Ex- "hello","123"
+# 2.integer : All the whole numbers are considered as integer data type Ex- 7427626
+# 3.float : Decimal numbers are considered as float data type. Ex- 1.33, 4.24
+
+
+# In[4]:Q4.Waht is an expression made up of? What do all expressions do?
+
+#Ans:
+#An expression is made up of operates and operands.All the expressions represents value.
+#For example any string is also an expressions since it represents the value of the string as well.
+
+
+# In[ ]:Q5.The assignment statements like spam = 10? What is the difference bw an expression and a statement?
+
+#Ans:
+#An expression is a combination of values and functions that are combined and interpreted to create a new value, where as an statement is a standalone unit  and does not return anything.
+
+#Q6.After running the following code, what does the variable bacon contain?
+# bacon = 22
+# bacon + 1
+
+#Ans:
+#23
+
+#Q7.What should the values of the following two terms be?
+#'spam'+'spamspam'
+#'spam'*3
+
+#Ans:
+#'spamspamspam'
+#'spamspamspam'
+
+#Q8. Why is eggs a valid variable name while 100 is invalid?
+
+#Ans:
+#Variable name can not being with a number.
+
+
+#Q9.What three functions can be used to get the integer, floating-point number, or string version of a value?
+#Ans:
+#int(), float(),str()
+
+#Q10.Why does this expression cause an error? How can you fix it?
+#'I have eaten' + 99 + 'burritos'
+
+#Ans:
+# Here we are trying to concatinate string with interger that why the expression will through an error
+# We can fix it by converting the interger to string the expresion will be like
+#'I have eaten'+'99'+'burritos'
+
